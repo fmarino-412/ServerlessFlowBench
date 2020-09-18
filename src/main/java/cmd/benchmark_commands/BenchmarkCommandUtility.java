7e68bb6804dc06e19a0fb1e5f242dcf25218d4e2
Francesco Marino <francesco.marino.412@students.uniroma2.eu>
2020-09-18 10:36:42 +0200

    Java DOC

--- src/main/java/cmd/benchmark_commands/BenchmarkCommandUtility.java
+++ src/main/java/cmd/benchmark_commands/BenchmarkCommandUtility.java
@@ -2,11 +2,27 @@ package cmd.benchmark_commands;
 
 import cmd.CommandUtility;
 
+/**
+ * Utility for wrk2 benchmark command execution
+ */
 public class BenchmarkCommandUtility extends CommandUtility {
 
+	/**
+	 * Docker image and command preamble
+	 */
 	private static final String WRK2_IMG = "bschitter/alpine-with-wrk2:0.1";
 	private static final String PREAMBLE = "docker" + SEP + "run" + SEP + "--rm" + SEP + WRK2_IMG;
 
+
+	/**
+	 * Builds wrk2 benchmark execution command
+	 * @param url url to perform benchmark on
+	 * @param concurrency number of concurrent requests
+	 * @param threads number of active threads
+	 * @param seconds test duration
+	 * @param requestsPerSecond number of requests per second
+	 * @return command as string
+	 */
 	public static String buildBenchmarkCommand(String url, Integer concurrency, Integer threads, Integer seconds,
 											   Integer requestsPerSecond) {
 		return	// command beginning
